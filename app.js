223f068bf4dc0da209a14451aa5133188f6846d3
Derek <nightwing917@hotmail.com>
2019-10-23 19:23:12 -0600

    adding req and res

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-let ourApp = http.createServer();
+// We are using an anonymous function for createServer because we don't need to call it anywhere else!
+// req = request and res = response
+let ourApp = http.createServer(function(req, res) {
+
+});
+
+ourApp.listen();
