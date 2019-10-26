a863d1d8949d2bb8f3264873cb51d64bc6078e46
Derek <nightwing917@hotmail.com>
2019-10-25 19:12:09 -0600

    comment out code to write for express

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-// Basic server
-let http = require("http");
-// We are using an anonymous function for createServer because we don't need to call it anywhere else!
-// req = request and res = response
-let ourApp = http.createServer(function(req, res) {
-    // console.log(req.url); 
-  // res.end('Hello, and welcome to our website!');
-  if(req.url == "/") {
-      res.end('Hello, and welcome to our website!');
-  }
+// // Basic server
+// let http = require("http");
+// // We are using an anonymous function for createServer because we don't need to call it anywhere else!
+// // req = request and res = response
+// let ourApp = http.createServer(function(req, res) {
+//     // console.log(req.url); 
+//   // res.end('Hello, and welcome to our website!');
+//   if(req.url == "/") {
+//       res.end('Hello, and welcome to our website!');
+//   }
 
-    if (req.url == "/about") {
-        res.end('Hello, and thank you for your interest in our company!');
-    }
+//     if (req.url == "/about") {
+//         res.end('Hello, and thank you for your interest in our company!');
+//     }
 
-    res.end('We cannot find the page you are looking for!');
+//     res.end('We cannot find the page you are looking for!');
 
-});
+// });
 
-// 3000 for port number NOTE: you have to use node app.js then in new window in browser type localhost:3000 for res.end response!
-// NOTE: listen will keep running in node once called remember ctrl c to stop it!
-ourApp.listen(3000);
+// // 3000 for port number NOTE: you have to use node app.js then in new window in browser type localhost:3000 for res.end response!
+// // NOTE: listen will keep running in node once called remember ctrl c to stop it!
+// ourApp.listen(3000);
+
+
+
+
+
+// Working with Express JS
