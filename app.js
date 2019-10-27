922101ed96318c7bb2b5095a15f9e6aaa6a5d2f5
Derek <nightwing917@hotmail.com>
2019-10-26 19:47:41 -0600

    finished req and res function

--- app.js
+++ app.js
@@ -25,10 +25,16 @@
 
 
 
-// Working with Express JS Server
+// Working with Express JS Server remember '/' is for local host 
 let express = require('express');
 let ourApp = express();
 ourApp,get('/', function(req, res) {
-
+    res.send(`
+       <form action="/answer" method="POST>
+        <p>What color is the sky on a clear and sunny day?</p>
+        <input name="skyColor" autocomplet="off">
+        <button>Submit Answer</button>
+       </form>
+    `);
 });
 ourApp.listen(3000);
